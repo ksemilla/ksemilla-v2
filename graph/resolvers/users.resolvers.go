5ea8bea582c283c49648318144832636d8c4d712
ksemilla <kevinsemilla@gmail.com>
2022-03-18 16:12:34 +0800

    test deploy

--- graph/resolvers/users.resolvers.go
+++ graph/resolvers/users.resolvers.go
@@ -40,7 +40,7 @@ func (r *queryResolver) FetchUser(ctx context.Context, id string) (*model.User, 
 }
 
 func (r *queryResolver) GetNumber(ctx context.Context) (int, error) {
-	return 2, nil
+	return 3, nil
 }
 
 // Mutation returns generated.MutationResolver implementation.
